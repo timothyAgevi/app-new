ad42238eef0efb5442a7eed7d34ae7360810c216
timothyAgevi <timothyagevi@gmail.com>
2022-01-07 15:07:37 +0300

    delete boiler code on appcomponent.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app-new';
-}
