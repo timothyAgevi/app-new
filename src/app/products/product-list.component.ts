38e0418225c5634db27b02a3becc73b0d1a07a39
timothyAgevi <timothyagevi@gmail.com>
2022-01-08 00:11:44 +0300

    ngIf

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,25 @@ templateUrl:'./product-list.component.html'
 export class ProductListComponent{
     pageTitle:string ='Product List !';
     products: any[]=[
-        
-    ]
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+          },
+          {
+            "productId": 8,
+            "productName": "Saw",
+            "productCode": "TBX-0022",
+            "releaseDate": "May 15, 2021",
+            "description": "15-inch steel blade hand saw",
+            "price": 11.55,
+            "starRating": 3.7,
+            "imageUrl": "assets/images/saw.png"
+          }
+    ];
 }
