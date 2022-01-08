20124fcf92ffcca05ac3f4d30a8ca43437ba0f7e
timothyAgevi <timothyagevi@gmail.com>
2022-01-08 11:47:18 +0300

    add FormsModule toimports array in NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { ProductListComponent } from './products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
