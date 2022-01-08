9005568cb9c4ec811c6b1a942275677b909f049e
timothyAgevi <timothyagevi@gmail.com>
2022-01-08 16:04:04 +0300

    add styleUrls

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
+
 import { IProduct } from "./product";
 
 @Component({
 selector:'pm-products',
-templateUrl:'./product-list.component.html'
-
+templateUrl:'./product-list.component.html',
+styleUrls:[ './product-list.component.css']
 })
 export class ProductListComponent{
     pageTitle:string ='Product List !';
