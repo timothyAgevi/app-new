6afc9e8e68a7192e3219724e4fd82758641d6831
timothyAgevi <timothyagevi@gmail.com>
2022-01-08 19:13:01 +0300

    add Transform method

--- src/app/shared/convert-to-spaces.pipe.ts
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -6,5 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 )
 export class ConvertToSpacesPipe implements PipeTransform{
-
+    transform(value: string, character: string): string {
+        return value.replace(character, ' ');
+      }
 }
