b920ff7d231d8d44be42dc3e93d296cdb7a3689a
timothyAgevi <timothyagevi@gmail.com>
2022-01-08 11:38:49 +0300

    add listFilter property

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -10,6 +10,7 @@ export class ProductListComponent{
     imageWidth:number=50;//add properties to class
     imageMargin:number=2;
     showImage:boolean=false;
+    listFilter:string='cart';
     products: any[]=[
         {
             "productId": 5,
