db686fdf45d978f28faa0323ba844180e3840d27
timothyAgevi <timothyagevi@gmail.com>
2022-01-10 16:12:39 +0300

    import HttpClintModule to app module

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -10,14 +10,14 @@ export class StarComponent implements OnChanges{//recalculate cropwidth based on
   //onChange
     @Input() rating:number=0; // input rating
   cropwidth:number=75;
-  @Output() ratingClicked  : EventEmitter <string>=
+  @Output()  ratingClicked: EventEmitter <string>=
   new EventEmitter();
 
   ngOnChanges(): void {
       this.cropwidth=this.rating * 75/5;
   }
   onClick(): void{
-      this.ratingClicked.emit(`the rating ${this.rating}was clicked`)
+      this.ratingClicked.emit(`  the rating ${this.rating} was clicked`)
 
   }
 }
