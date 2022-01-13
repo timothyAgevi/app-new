ab37758df1184dc21b05acc10f6d55aabef70fa2
timothyAgevi <timothyagevi@gmail.com>
2022-01-13 11:31:50 +0300

    delete pm-products from appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ProductListComponent } from "./products/product-list.component";
   selector:'pm-root',
   template: `<div>
   <h1>{{pageTitle}}</h1>
-<pm-products> </pm-products></div>`
+`
   
 
 })
