e269412ad11ed8e822e41ff712e8c240fa0e63e8
timothyAgevi <timothyagevi@gmail.com>
2022-01-13 11:44:23 +0300

    add menu bar to app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from "@angular/core";
 import { ProductListComponent } from "./products/product-list.component";
 @Component({
   selector:'pm-root',
-  template: `<div>
-  <h1>{{pageTitle}}</h1>
+  template: `<nav class='navbar navbar-expand navbar-light bg-light'>
+  <a class='navbar-brand'>{{pageTitle}}</a>
+  <ul class='nav nav-pills'>
+    <li><a class='nav-link' routerLinkActive='active' routerLink='/welcome'>Home</a></li>
+    <li><a class='nav-link' routerLinkActive='active' routerLink='/products'>Product List</a></li>
+  </ul>
+</nav>
 `
   
 
