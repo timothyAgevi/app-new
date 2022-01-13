f8562d3bbe7fbd46ed19aedeb11a8c68bf7a853f
timothyAgevi <timothyagevi@gmail.com>
2022-01-13 19:02:08 +0300

    add constructor for guard

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDetailGuard implements CanActivate {
+
+
+  constructor(private router:Router){}
+  
   canActivate(
     route: ActivatedRouteSnapshot,//provide current route info
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
