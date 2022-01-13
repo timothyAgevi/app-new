a569700cec3ced8191671f4c874ed0381c9ee6d0
timothyAgevi <timothyagevi@gmail.com>
2022-01-14 00:19:00 +0300

    add feature module product data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+
 import {HttpClientModule} from '@angular/common/http';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailComponent } from './products/product-detail.component';
+
 import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
+    
     WelcomeComponent,
-    ProductDetailComponent     
+         
     
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    
     HttpClientModule,
     RouterModule.forRoot([
       {path:'products',component:ProductListComponent},
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './product-detail.component';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ]
 })
 export class ProductModule { }
