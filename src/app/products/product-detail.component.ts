b5220078db035c59d2cc8bc1fabe35882947c2b1
timothyAgevi <timothyagevi@gmail.com>
2022-01-13 15:52:34 +0300

    add ActivatedRoute

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle:string='Product Detail';
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
